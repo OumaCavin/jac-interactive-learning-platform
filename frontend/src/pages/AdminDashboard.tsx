f4d05dcfaef35ef59b6fbf9ecc0fe56e23218b57
OumaCavin <cavin.otieno012@gmail.com>
2025-11-23 07:03:33 +0800

    fix(frontend): correct AdminDashboard store import path to ../store/store

--- frontend/src/pages/AdminDashboard.tsx
+++ frontend/src/pages/AdminDashboard.tsx
@@ -14,7 +14,7 @@ import {
   ClockIcon,
 } from '@heroicons/react/24/outline';
 import { useSelector } from 'react-redux';
-import { RootState } from '../../store/store';
+import { RootState } from '../store/store';
 
 interface AdminStats {
   totalUsers: number;
