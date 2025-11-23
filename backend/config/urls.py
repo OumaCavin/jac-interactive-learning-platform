d7a5dae2b101f7ac832c68a1fb8e77c3602e4854
OumaCavin <cavin.otieno012@gmail.com>
2025-11-23 22:00:31 +0800

    Fix missing timezone import in urls.py health endpoint

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -9,6 +9,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.http import JsonResponse
+from django.utils import timezone
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
